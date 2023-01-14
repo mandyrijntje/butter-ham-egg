aef8d378e10123bb691732b280f9b404506715e4
Bubu <msmandym@protonmail.com>
2023-01-14 19:24:57 +0100

    Creation function to handle user clicking a box

--- src/App.js
+++ src/App.js
@@ -7,14 +7,28 @@ import {useState} from 'react';
 const numberOfBoxes = () => (new Array(9)).fill(null);
 
 function App() {
+  
   const [boxes, setBoxes] = useState(numberOfBoxes);
+
+  function handleBoxClick(index) {
+
+    var newBoxes = boxes; //make a new array of boxes to mark the 'clicked index' box with an X
+
+    newBoxes[index] = 'X'; //put an X in array, at the index of clicked box
+
+    setBoxes([...newBoxes]); //passing the new array as arg in the useState fn, so it can be reused
+
+  }
 
   return (
     <main>
       <Header />
       <Game>
         {boxes.map((box, index) => 
-        <Box />
+
+        <Box onClick = {()=> handleBoxClick(index)} //passing the index of clicked box to fn, so it can do something with it. In this case, display x
+
+        />
         )
        }
       </Game>
