3ddd17c34d00513260fa228bf0fbeb48a2bc7939
Bubu <msmandym@protonmail.com>
2023-01-17 03:07:31 +0100

    Added colors and symbols to gameplay

--- src/Box.js
+++ src/Box.js
@@ -1,8 +1,16 @@
+const xStyle = {
+    color: 'orange'
+}
+
+const oStyle = {
+    color: 'white'
+}
+
 function Box(props) {
     return(
         <div className = {'box'}{...props}>
-            <span className='symbol'>
-                { props.x ? 'X' : props.o ? 'O' : null}
+            <span className='symbol' style={props.x ? xStyle : oStyle}>
+                { props.x ? '■' : props.o ? '⬮' : null}
             </span>
         </div>
     );
